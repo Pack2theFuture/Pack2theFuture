8e5581a9c1b898542a85d51bae80230f954fe63d
oneusesol <rbqls000328@hanyang.ac.kr>
2025-05-26 21:08:01 +0900

    render2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from 'tailwindcss'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),tailwindcss()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-    server: {
-    port: 10000, // 원하는 포트 번호로 설정
+  plugins: [react()],
+  build: {
+    outDir: 'dist',  // Vite 기본값이지만 명시해줘도 좋아요
   },
-});
+  server: {
+    port: 3000, // 로컬 개발용 포트 (Render와 무관)
+  },
+  base: '/', // 경로 기준 설정 (Render는 기본 / 로 하면 돼요)
+})
