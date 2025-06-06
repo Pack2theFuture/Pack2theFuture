6168188fd501aa6ebc90c025336da4d1d892f9cb
김효진 <dlapdlffldpald1@naver.com>
2025-06-07 03:35:43 +0900

    add slidebar

--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -3,12 +3,13 @@ import KakaoMap from "./KakaoMap";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
+
 function Map() {
   return (
 <>
 <Header/>
 <KakaoMap/>
-<Footer/>
+{/* <Footer/> */}
 </>
   );
 }
